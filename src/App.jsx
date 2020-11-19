f0b28213e400a6199be2cbcd82435eb1ba98e543
shinhonggyu <zowoz0@naver.com>
2020-11-19 18:50:06 +0900

    Transaction

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import styles from './App.module.css';
 import Header from './components/Header';
 import Balance from './components/Balance';
-import IncomeExpenses from './components/incomeExpense/IncomeExpenses';
+import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
+
+import './App.css';
 
 const App = () => {
   return (
     <div>
       <Header />
-      <div className={styles.container}>
+      <div className="container">
         <Balance />
         <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
       </div>
     </div>
   );
---
+++ src/components/AddTransaction.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+const AddTransaction = () => {
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState(0);
+
+  return (
+    <>
+      <h3>Add new transaction</h3>
+      <form>
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input
+            type="text"
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter text..."
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">
+            Amount <br />
+            (negative - expense, positive - income)
+          </label>
+          <input type="number" placeholder="Enter amount..." />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+    </>
+  );
+};
+
+export default AddTransaction;
